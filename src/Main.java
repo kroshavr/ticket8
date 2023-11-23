0929c324110694f62d917e7b2c81daaca6192342
krosh <kroshavr@mail.ru>
2023-11-23 17:53:44 +0500

    Задача из билета №8

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
 public class Main {
-public static void main(String[] args) {
+    public static void main(String[] args) {
         try {
-        String str = check(" ");
-        System.out.println(str);
+                String str = check(" ");
+                System.out.println(str);
         }
         catch (MyException e) {
-        e.printStackTrace();
+                e.printStackTrace();
         }
-        }
+    }
 
-
-public static String check (String str) throws MyException {
+    public static String check (String str) throws MyException {
         if (str == null || str.isBlank() || str.isEmpty()) {
-        throw new MyException ("Ошибка");
+                throw new MyException ("Ошибка");
         }
         else return str;
-        }
+    }
 }
